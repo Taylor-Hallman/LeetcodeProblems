c98b04b25e7c178067ecaa9df8ba1c5c0ff6a5cb
Taylor-Hallman <105246995+Taylor-Hallman@users.noreply.github.com>
2026-01-20 14:19:47 -0600

    Solve House Robber (#198)

---
+++ Leetcode/HouseRobber.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+class Solution {
+public:
+    std::unordered_map<int, int> map;
+
+    int rob(std::vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1)
+            return nums[0];
+
+        std::vector<int> dp(n);
+        
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = std::invoke([&]() {
+                int p1 = i + 2 < n ? nums[i] + dp[i + 2] : nums[i];
+                int p2 = i + 3 < n ? nums[i] + dp[i + 3] : nums[i];
+                return std::max(p1, p2);
+            });
+        }
+
+        return std::max(dp[0], dp[1]);
+    }
+};
